c53bdc107626f7b11befc1e7e98e2519e74a3912
Eric <eric.morales@protonmail.com>
2022-03-02 17:41:15 -0400

    refactr(navigation): add component for the navigation links

--- src/components/LeftContainer/LeftContainer.js
+++ src/components/LeftContainer/LeftContainer.js
@@ -1,33 +1,21 @@
 import './LeftContainer.css';
 import About from '../About/About.js';
+import Navigation from '../Navigation/Navigation';
 import image from '../../resources/profile.jpg';
 
 function LeftContainer() {
     return (
         <div id="left-side-container">
             <div id="upper-left-container">
-                    <About title="Hello, I'm Eric Morales." 
+                    <About 
+                    title="Hello, I'm Eric Morales." 
                     description="I’m a prospect Software Engineer currently studying at Make School inside the heart of San Francisco, California. I most likely will be specialized on data science, and iOS mobile development by fall of 2022. I can't say I'm a full fletch Software Engineer yet. Nevertheless, that won't stop me from letting you know about my skills. That's why I will be sharing my biggest projects at the moment and some of my skills that can be put to use here."
                     />
-                    <nav id="navigator">
-                        <ul id="ul-nav">
-                            <li id="projects-link">
-                                <a href="#projects">
-                                    <span id="project-text">Projects</span>
-                                </a>
-                            </li>
-                            <li id="skills-link">
-                                <a href="#skills">
-                                    <span id="skills-text">Skills</span>
-                                </a>
-                            </li>
-                            <li id="contact-link">
-                                <a href="#contact">
-                                    <span id="contact-text">Contact</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+
+                    <Navigation 
+                    titles={['Projects', 'Skills']}
+                    hrefs={['#projects', '#skills']}
+                    />
             </div>
             <div id="bottom-left-container">
                 <img src={image} alt="Eric's with blur face."/>
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import './Navigation.css';
+
+function Navigation(props) {
+    const { titles, hrefs } = props
+
+    return (
+        <nav id="navigator">
+        <ul id="ul-nav">
+            {
+                titles.map((title, index) => (
+                    <li>
+                        <a href={hrefs[index]}>
+                            <span id="span-title">{title}</span>
+                        </a>
+                    </li>  
+                ))
+            }
+        </ul>
+    </nav>
+    )
+}
+
+export default Navigation
